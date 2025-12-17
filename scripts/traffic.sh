fb81d3a5791f29b9788b369b46013d0caffdd23f
wwy-1208 <2952651525@qq.com>
2025-12-17 15:18:16 +0800

    Add files via upload

---
+++ scripts/traffic.sh
@@ -0,0 +1,52 @@
+root_path_name=./data/traffic
+data_path_name=traffic.csv
+model_id_name=traffic
+data_name=custom
+
+gpu=6
+random_seed=2021
+model_name=DP-Mamba
+label_len=336
+seq_len=$(($label_len*2))
+
+for pred_len in 96 192 336 720
+do
+  python -u run.py \
+    --random_seed $random_seed \
+    --is_training 1 \
+    --root_path $root_path_name \
+    --data_path $data_path_name \
+    --model_id $model_id_name'_'$seq_len'_'$label_len'_'$pred_len \
+    --model $model_name \
+    --data $data_name \
+    --features M \
+    --seq_len $seq_len \
+    --label_len $label_len \
+    --pred_len $pred_len \
+    --enc_in 862 \
+    --dec_in 862 \
+    --c_out 862 \
+    --embed_type 4 \
+    --m_layers 1 \
+    --d_state 16 \
+    --d_conv 4 \
+    --e_layers 3 \
+    --n_heads 8 \
+    --d_model 64 \
+    --d_ff 128 \
+    --dropout 0.3 \
+    --fc_dropout 0.3 \
+    --head_dropout 0 \
+    --fft_top_k 2 \
+    --patch_len 16 \
+    --stride 16 \
+    --concat 1 \
+    --train_epochs 10 \
+    --batch_size 8 \
+    --learning_rate 0.0001 \
+    --patience 10 \
+    --pct_start 0.2\
+    --des 'Exp' \
+    --gpu $gpu \
+    --itr 1 >>logs/Forecasting/$model_name'_'$model_id_name'_'$seq_len'_'$label_len'_'$pred_len.log 
+done
